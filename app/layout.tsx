0749087e629739da766ead1863b5de725729e0cb
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-03-30 00:50:42 +0500

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import "./globals.css"
 
 export const metadata = {
-  title: "Dangasalikdan Halos Bo'l - Jonli Vebinar",
+  title: "Maqsadlarga erishish - Jonli Vebinar",
   description: "Jonli bepul vebinar davomida dangasalikdan qutilish va maqsadlarga erishish yo'llari",
 }
 
